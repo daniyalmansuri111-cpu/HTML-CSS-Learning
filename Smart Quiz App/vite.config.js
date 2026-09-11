d9023690ce2cad9175b05e51d9c7af9a64794558
DANIYAL PINJARI <daniyalmansuri111@gmail.com>
2026-09-11 16:20:50 +0530

    Configure Smart Quiz base path for GitHub Pages

---
+++ Smart Quiz App/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/HTML-CSS-Learning/Smart-Quiz-App/',
+  plugins: [react()],
+})
